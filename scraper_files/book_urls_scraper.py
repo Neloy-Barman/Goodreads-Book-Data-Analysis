743ec24a1016efe9637a859704eb34dc30ed1d2c
Neloy Barman <neloycareer018@gmail.com>
2024-01-02 23:18:23 +0600

    new details added

--- scraper_files/book_urls_scraper.py
+++ scraper_files/book_urls_scraper.py
@@ -11,7 +11,8 @@ def main():
     # base_url = "https://www.goodreads.com/list/show/1043.Books_That_Should_Be_Made_Into_Movies"
     # base_url = "https://www.goodreads.com/list/show/1.Best_Books_Ever"
     # base_url = "https://www.goodreads.com/list/show/94.Books_With_Unforgettable_Characters"
-    base_url = "https://www.goodreads.com/list/show/19106.MUST_READS_"
+    # base_url = "https://www.goodreads.com/list/show/19106.MUST_READS_"
+    base_url = "https://www.goodreads.com/list/show/423.Books_That_Exceeded_Your_Expectations"
 
     driver = webdriver.Chrome()
     
@@ -43,7 +44,7 @@ def main():
                 continue
 
         df = pd.DataFrame(columns=columns, data=book_urls)
-        df.to_csv("book_urls_must_reads.csv", index=False)
+        df.to_csv("url_files/book_urls_exceeded_Expectations.csv", index=False)
 
 if __name__ == "__main__":
     main()
